aef0787bd3f7be20446396349411b65f61ccf732
Rahul P <57433183+rahulpoojith@users.noreply.github.com>
2024-12-21 00:04:41 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
+import nltk
+nltk.download()
+
 
 # Ensure NLTK data is downloaded
 nltk.download('punkt')
